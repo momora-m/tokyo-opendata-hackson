d8f4b6a322ce10ec6638fae85942cd3f5f4f1ca7
ksera524 <ksera631@gmail.com>
2022-08-20 16:19:15 +0900

    CallしたらJson返すところまで

--- backend/tokyo-opendata-backend/src/main.rs
+++ backend/tokyo-opendata-backend/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get,HttpResponse, HttpServer,App};
+use serde::{Deserialize, Serialize};
+
+use std::{fs::File, io::BufReader};
+
+const SERVER:&str = "0.0.0.0:8080";
+const JSON_NAME:&str = "test.json";
+
+#[derive(Serialize, Deserialize)]
+struct ReturnJson{
+    q1:i32,
+    q2:i32,
+    q3:i32,
+    q4:i32,
+    q5:i32,
+}
+
+#[get("/getJson")]
+async fn get_json() -> Result<HttpResponse,actix_web::Error>{
+    let json = File::open(JSON_NAME).unwrap();
+    let json = BufReader::new(json);
+
+    let res:ReturnJson = serde_json::from_reader(json).unwrap();
+
+        Ok(
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(res))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .service(get_json)
+    })
+    .bind(SERVER)?
+    .run()
+    .await
 }
