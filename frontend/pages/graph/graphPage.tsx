402b238d1ad6f1fa14f3708192097ece0e76b5e3
momora-m <kai10mishina@gmail.com>
2022-08-23 21:25:44 +0900

    デバッガーの設定とjson取得処理一旦追加

--- frontend/pages/graph/graphPage.tsx
+++ frontend/pages/graph/graphPage.tsx
@@ -1,9 +1,39 @@
+import { getServers } from 'dns';
 import type { NextPage } from 'next'
+import Head from 'next/head';
 
 
-const GraphPage: NextPage = () => {
+
+export async function getServerSideProps () {
+  try {
+      const host = 'localhost:8080'
+      const products = await fetch(`http://localhost:8080/getJson`)
+          .then(data => data.json())
+      return {
+          props: {
+              products,
+          }
+      }
+  } catch (e) {
+      console.log(e)
+      return {
+          props: {
+              products: [],
+          }
+      }
+  }
+}
+
+const GraphPage: NextPage = (props) => {
   return (
-    <h1>First Post</h1>
+      <div>
+          <Head>
+              <title>Graph Page</title>
+              <meta name="description" content="検索エンジン用の説明文" />
+          </Head>
+          <h1>Graph Page</h1>
+           <pre><code>{JSON.stringify(props,null,2)}</code></pre>
+      </div>
   )
 }
 
